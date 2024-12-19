ccb43413277b504ee227c5dcb25df5d311d0bf86
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-19 18:12:17 +0600

    feat(blog): add Zod validation schemas for blog creation and update

---
+++ src/app/modules/Blog/blog.validation.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const createBlogValidationSchema = z.object({
+    body: z.object({
+        title: z.string({ required_error: "Title is required!" }),
+        content: z.string({ required_error: "Content is required!" }),
+        author: z.string({ required_error: "Author ID is required!" })
+    })
+})
+
+const updateBlogValidationSchema = z.object({
+    body: z.object({
+        title: z.string({ required_error: "Title is required!" }).optional(),
+        content: z.string({ required_error: "Content is required!" }).optional(),
+        author: z.string({ required_error: "Author ID is required!" }).optional()
+    })
+})
+
+export const blogValidationSchema = {
+    createBlogValidationSchema,
+    updateBlogValidationSchema,
+}
