8a1d0f961f1411c8f58b94819443575832688e78
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-19 19:11:14 +0600

    feat(blog): add getAllBlogsFromDB function to fetch all blogs from database

--- src/app/modules/Blog/blog.service.ts
+++ src/app/modules/Blog/blog.service.ts
@@ -2,10 +2,16 @@ import { TBlog } from './blog.interface';
 import { Blog } from './blog.model';
 
 const createBlogIntoDB = async (payload: TBlog) => {
-  const result = await Blog.create(payload);
-  return result;
+    const result = await Blog.create(payload);
+    return result;
 };
 
+const getAllBlogsFromDB = async () => {
+    const result = await Blog.find();
+    return result;
+}
+
 export const BlogServices = {
-  createBlogIntoDB,
+    createBlogIntoDB,
+    getAllBlogsFromDB,
 };
