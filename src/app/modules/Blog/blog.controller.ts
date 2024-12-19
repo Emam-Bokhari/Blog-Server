e71d881df5bdc274fed73f6f2d627077bdfbb8ec
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-19 18:44:48 +0600

    refactor(blog): use sendRespnse for response in blog creation request handler

--- src/app/modules/Blog/blog.controller.ts
+++ src/app/modules/Blog/blog.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import { BlogServices } from "./blog.service"
+import { sendResponse } from "../../utils/sendResponse";
 
 const createBlog = async (req: Request, res: Response) => {
     const result = await BlogServices.createBlogIntoDB(req.body);
 
-    res.status(201).json({
+    sendResponse(res, {
+        statusCode: 201,
         success: true,
         message: "Blog created successfully",
-        statusCode: 201,
         data: result,
     })
 
