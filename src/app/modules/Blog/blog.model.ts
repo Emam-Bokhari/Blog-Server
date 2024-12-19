b50e22dd312e93986bbb52a75319fe16d61deaa0
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-19 18:04:02 +0600

    feat(blog): add Blog model with schema defination

--- src/app/modules/Blog/blog.model.ts
+++ src/app/modules/Blog/blog.model.ts
@@ -1,1 +1,24 @@
-const 
+import { model, Schema } from "mongoose";
+import { TBlog } from "./blog.interface";
+
+const blogSchema = new Schema<TBlog>({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    author: {
+        type: Schema.ObjectId,
+        required: true,
+        // ref: "users"
+    }
+},
+    { timestamps: true }
+)
+
+export const Blog = model("Blog", blogSchema)
