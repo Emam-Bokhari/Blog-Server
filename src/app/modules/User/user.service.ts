69251af047f7960f6403bc8dae6ac0ed0b108537
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-20 08:52:51 +0600

    feat(error): implement custom AppError class for error handling

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -2,6 +2,13 @@ import { TUser } from "./user.interface";
 import { User } from "./user.model"
 
 const registerUserIntoDB = async (payload: TUser) => {
+
+    const isEmailExists = await User.findOne({ email: payload?.email })
+
+    if (isEmailExists) {
+        throw new Error("Email is already exists!")
+    }
+
     const result = await User.create(payload);
 
     return result;
