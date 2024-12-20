05068839e51d330b56d60a209d7a84a344878aba
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-20 08:45:08 +0600

    feat(user): hashed password before saving the database

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,5 +1,7 @@
 import { model, Schema } from 'mongoose';
 import { TUser } from './user.interface';
+import bcrypt from "bcrypt"
+import config from '../../config';
 
 export const userSchema = new Schema<TUser>({
     name: {
@@ -32,5 +34,15 @@ export const userSchema = new Schema<TUser>({
         timestamps: true,
     }
 )
+
+// hashed password before save the database
+userSchema.pre("save", async function (next) {
+
+    this.password = await bcrypt.hash(this.password, Number(config.bcrypt_salt_rounds)
+    )
+    next()
+})
+
+
 
 export const User = model<TUser>("User", userSchema);
