bcfa39322bebcde64699a15eba5093f80ce36db0
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-20 08:24:37 +0600

    feat: add route for user registration

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { BlogRoutes } from "../modules/Blog/blog.route";
+import { UserRoutes } from "../modules/User/user.route";
 
 export const router = express.Router();
 
@@ -7,6 +8,10 @@ const moduleRoutes = [
     {
         path: "/blogs",
         route: BlogRoutes,
+    },
+    {
+        path: "/auth",
+        route: UserRoutes,
     }
 ];
 
