0a3f519d67bb6d0c16804831cb79d8d0842b76f7
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-20 09:53:18 +0600

    feat(user): define USER_ROLE and TuserRole for role management

--- src/app/modules/User/user.interface.ts
+++ src/app/modules/User/user.interface.ts
@@ -1,4 +1,5 @@
 import { Model } from "mongoose";
+import { USER_ROLE } from "./user.constant";
 
 
 export interface TUser {
@@ -12,3 +13,5 @@ export interface TUser {
 export interface UserModel extends Model<TUser> {
     isUserExists(email: string): Promise<TUser>;
 }
+
+export type TUserRole = keyof typeof USER_ROLE
